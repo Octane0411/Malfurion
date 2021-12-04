b4f5b67d72e7f43826d81c9b8eaf5c6f2f210eb3
Octane0411 <wdznb1@gmail.com>
2021-12-04 20:56:36 +0800

    init

---
+++ malfurion-server/src/main/java/com/malfurion/malfurionserver/MalfurionServerApplication.java
@@ -0,0 +1,13 @@
+package com.malfurion.malfurionserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalfurionServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MalfurionServerApplication.class, args);
+    }
+
+}
---
+++ malfurion-server/src/test/java/com/malfurion/malfurionserver/MalfurionServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.malfurion.malfurionserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalfurionServerApplicationTests {
+
+    @Test
+    void testDataBaseConnection() {
+
+    }
+
+}
